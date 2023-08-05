e680196f0b2bc8a712417644a412ab95d772e416
ninjashenoy <140610275+ninjashenoy@users.noreply.github.com>
2023-08-05 19:23:02 +0530

    Update app.js

--- src/js/app.js
+++ src/js/app.js
@@ -19,7 +19,7 @@ document.addEventListener("DOMContentLoaded", function () {
         "type": "polygon",
         "stroke": {
           "width": 0,
-          "color": "#000000"
+          "color": "#d28816"
         },
         "polygon": {
           "nb_sides": 5
